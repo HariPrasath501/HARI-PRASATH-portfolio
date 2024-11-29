28284cf2b16271a44ed2261aff939a2368d74cb4
HariPrasath501 <HariPrasath501502@gamil.com>
2024-11-29 18:16:07 +0530

    feat:frist commit

---
+++ JS/main.js
@@ -0,0 +1,63 @@
+// Navbar Toggle for Mobile
+
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuicon.onclick = () => {
+    menuicon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+// Scroll Section Active Link
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Sticky Navbar
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove Toggle Icon and Navbar Active on Scroll
+
+    menuicon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// Scroll Reveal
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+// Typed.js
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
